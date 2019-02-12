d42384edf292ee6a25c9eb56019e56fb12928788
Jacob Howard <jacob@havoc.io>
2019-02-12 15:36:05 +0200

    Switched Chmod to use resolved SIDs.
    The Trustee structure doesn't seem to support using SIDs (in string
    format) in the name parameter. This commit changes the Chmod function to
    use parsed SID objects instead.

--- chmod.go
+++ chmod.go
@@ -2,7 +2,34 @@ package acl
 
 import (
 	"os"
+
+	"golang.org/x/sys/windows"
 )
+
+var creatorOwnerSid, creatorGroupSid, everyoneSid *windows.SID
+
+func init() {
+	// Create the CREATOR OWNER well-known SID.
+	if s, err := windows.StringToSid("S-1-3-0"); err != nil {
+		panic("unable to create CREATOR OWNER well-known SID")
+	} else {
+		creatorOwnerSid = s
+	}
+
+	// Create the CREATOR GROUP well-known SID.
+	if s, err := windows.StringToSid("S-1-3-1"); err != nil {
+		panic("unable to create CREATOR GROUP well-known SID")
+	} else {
+		creatorGroupSid = s
+	}
+
+	// Create the EVERYONE well-known SID.
+	if s, err := windows.StringToSid("S-1-1-0"); err != nil {
+		panic("unable to create EVERYONE well-known SID")
+	} else {
+		everyoneSid = s
+	}
+}
 
 // Change the permissions of the specified file. Only the nine
 // least-significant bytes are used, allowing access by the file's owner, the
@@ -12,8 +39,8 @@ func Chmod(name string, mode os.FileMode) error {
 		name,
 		true,
 		false,
-		GrantName((uint32(mode)&0700)<<23, "S-1-3-0"),
-		GrantName((uint32(mode)&0070)<<26, "S-1-3-1"),
-		GrantName((uint32(mode)&0007)<<29, "S-1-1-0"),
+		GrantSid((uint32(mode)&0700)<<23, creatorOwnerSid),
+		GrantSid((uint32(mode)&0070)<<26, creatorGroupSid),
+		GrantSid((uint32(mode)&0007)<<29, everyoneSid),
 	)
 }
